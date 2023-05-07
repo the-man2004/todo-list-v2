14fea430ff97ea7b05639645507bdb3f514d7bd7
keebot-png <kee.bot@icloud.com>
2023-05-07 17:59:23 +0200

    Add webpack and add basic styling.

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'To Do list',
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+}
