30ddf2ababc8d30ff3958390c05b9e2194b1fff3
the-man2004 <heinrichhb111@gmail.com>
2023-05-07 18:28:08 -0700

    Added add todo functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello world!");
+const form = document.querySelector(".add-text");
+const addInput = document.querySelector(".add-input");
+const todoContainer = document.querySelector(".todos");
+
+// Helper functions
+const addTodo = (e) => {
+  todoContainer.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <li>
+        <input type="checkbox" />
+        <p>${e}</p>
+        <p>edit</p>
+        <p>delete</p>
+    </li>
+    `
+  );
+};
+
+// EventListeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  addTodo(addInput.value);
+  console.log(addInput.value);
+});
