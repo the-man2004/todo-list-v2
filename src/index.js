53ae78057453653fa63fb8ba77fbc90fb6fadce7
the-man2004 <heinrichhb111@gmail.com>
2023-05-07 20:00:41 -0700

    Added renderTodo function

--- src/index.js
+++ src/index.js
@@ -2,19 +2,53 @@ const form = document.querySelector(".add-text");
 const addInput = document.querySelector(".add-input");
 const todoContainer = document.querySelector(".todos");
 
+// Global variables
+const obj = [
+  {
+    title: "yes sir",
+    id: 1,
+    checked: true,
+  },
+  {
+    title: "Good day to you",
+    id: 2,
+    checked: true,
+  },
+  {
+    title: "NOOOOOOOOOOO",
+    id: 3,
+    checked: false,
+  },
+];
+
 // Helper functions
-const addTodo = (e) => {
+const renderTodo = (obj) => {
+  obj.forEach((todo) => addTodo(todo));
+};
+
+const addTodo = (todo) => {
   todoContainer.insertAdjacentHTML(
     "afterbegin",
     `
-    <li>
-        <input type="checkbox" />
-        <p>${e}</p>
-        <p>edit</p>
-        <p>delete</p>
-    </li>
+      <li id="${todo.id}">
+          ${
+            !todo.checked
+              ? '<input type="checkbox" />'
+              : '<input checked type="checkbox" />'
+          }
+          <p>Item three</p>
+          <button class="edit-button">edit</button>
+          <button>delete</button>
+      </li>
     `
   );
+};
+
+const editTodo = (id, p) => {
+  const el = document.getElementById(id);
+  console.log(el);
+
+  console.log(id);
 };
 
 // EventListeners
@@ -23,4 +57,16 @@ form.addEventListener("submit", function (e) {
 
   addTodo(addInput.value);
   console.log(addInput.value);
+});
+
+todoContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("edit-button")) {
+    editTodo(e.target.parentNode.id);
+
+    console.log("edit");
+  }
+});
+
+window.addEventListener("load", function () {
+  renderTodo(obj);
 });
